fc7f14fd138473b3044f5d86209b3227fcd80447
Yoshan Mumberson <mryosh@outlook.com>
2024-03-25 20:57:59 +0000

    Finished styling the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,15 +19,20 @@ const Navbar = () => {
       <header>
         <nav className="navbar">
           <a href="#welcome" className="navMenuText">TODO_LOGO</a>
-          <ul className="navMenu">
+          <ul className={`navMenu ${isActive ? "active" : ""}`} >
             {navLinks.map((link) => {
               return (
-                <li key={link.id}>
+                <li key={link.id} onClick={removeActive}>
                   <a href={`#${link.id}`} className="navMenuText">{link.title}</a>
                 </li>
               );
             })}
           </ul>
+          <div className={`hamburger ${isActive ? "active" : ""}`} onClick={toggleActiveClass}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
         </nav>
       </header>
     </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -21,24 +21,24 @@ export const navLinks = [
         id: "partners",
         title: bulgarian ? "Партньори" : "Partners",
     },
-    // {
-    //     id: "achievements",
-    //     title: bulgarian ? "Постижения" : "Achievements",
-    // },
-    // {
-    //     id: "gallery",
-    //     title: bulgarian ? "Галерия" : "Gallery",
-    // },
+    {
+        id: "achievements",
+        title: bulgarian ? "Постижения" : "Achievements",
+    },
+    {
+        id: "gallery",
+        title: bulgarian ? "Галерия" : "Gallery",
+    },
     // {
     //     id: "legal",
     //     title: bulgarian ? "Legal" : "Legal",
     // },
-    // {
-    //     id: "goals",
-    //     title: bulgarian ? "Цели" : "Goals",
-    // },
-    // {
-    //     id: "contacts",
-    //     title: bulgarian ? "Контакти" : "Contacts",
-    // },
+    {
+        id: "goals",
+        title: bulgarian ? "Цели" : "Goals",
+    },
+    {
+        id: "contacts",
+        title: bulgarian ? "Контакти" : "Contacts",
+    },
 ]
