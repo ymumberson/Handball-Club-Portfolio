90b46126c094535a69108a0641a2e834345fc1a9
Yoshan Mumberson <mryosh@outlook.com>
2024-04-13 20:19:47 +0100

    Resolved scaling issues.

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -3,47 +3,33 @@ import { partners } from "../constants";
 import { partnersNoImage } from "../constants";
 
 const PartnerImage = (props) => {
-    return (
-        <div className="">
-            <img className="partner-image" src={props.src} alt={props.alt}/>
-        </div>
-    )
-}
+  return <img className="partner-image" src={props.src} alt={props.alt} />;
+};
 
 const PartnerText = (props) => {
-    return (
-        <div>
-            <h2>{props.text}</h2>
-        </div>
-    )
-}
+  return <h2>{props.text}</h2>;
+};
 
 const Partners = () => {
-    return (
-        <div>
-            <h1 className="section-header">{navLinks.find((item) => item.id === "partners").title}</h1>
-            <div className="partners-section">
-                <div className="partners-image-container">
-                    {
-                        partners.map((partner) => {
-                            return (
-                                <PartnerImage src={partner.img} alt={partner.name}/>
-                            )
-                        })
-                    }
-                </div>
-                <div className="partners-text-container">
-                {
-                        partnersNoImage.map((partner) => {
-                            return (
-                                <PartnerText text={partner.name}/>
-                            )
-                        })
-                    }
-                </div>
-            </div>
+  return (
+    <div>
+      <h1 className="section-header">
+        {navLinks.find((item) => item.id === "partners").title}
+      </h1>
+      <div className="partners-section">
+        <div className="partners-image-container">
+          {partners.map((partner) => {
+            return <PartnerImage src={partner.img} alt={partner.name} />;
+          })}
         </div>
-    )
-}
+        <div className="partners-text-container">
+          {partnersNoImage.map((partner) => {
+            return <PartnerText text={partner.name} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Partners;
