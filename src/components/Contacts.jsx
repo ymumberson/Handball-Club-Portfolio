5dca71ea7e328ed01510b4e10d7ba4aff5441f9c
Yoshan Mumberson <mryosh@outlook.com>
2024-04-14 15:31:53 +0100

    Finished contacts section.

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -7,7 +7,7 @@ const Contacts = () => {
       <h1 className="section-header">
         {navLinks.find((item) => item.id === "contacts").title}
       </h1>
-      <div>
+      <div className="contacts-section">
         {contact.map((contact) => {
           return (
             <Contact
@@ -26,20 +26,23 @@ const Contact = (props) => {
   return (
     <div className="contact-container">
       <h2 className="subsection-header">{props.name}</h2>
-      {props.email && (
-        <p>
-          <i class="material-icons">email</i>
-          {props.email}
-        </p>
-      )}
-      {props.phone && (
-        <p>
-          <i class="material-icons">phone</i>
-          {props.phone}
-        </p>
-      )}
+      <ContactComponent iconKey="email" contactValue={props.email} />
+      <ContactComponent iconKey="phone" contactValue={props.phone} />
     </div>
   );
+};
+
+const ContactComponent = (props) => {
+  if (props.contactValue) {
+    return (
+      <p className="contact-component-container">
+        <i class="material-icons">{props.iconKey}</i>
+        {props.contactValue}
+      </p>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Contacts;
