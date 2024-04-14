3bae243eb2c7b28bf29f7ee3c7a68b51f1d25b11
Yoshan Mumberson <mryosh@outlook.com>
2024-04-14 16:37:05 +0100

    Added functionality to navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import background from "./assets/pexels-codioful-formerly-gradienta-7130534.jpg";
 
 import {
@@ -20,18 +20,35 @@ import {
 function App() {
   return (
     // <div style={{backgroundImage: `url(${background})`}}>
-    <div style={{backgroundColor: "honeydew"}}>
-      <Navbar />
-      <Welcome />
-      <About />
-      <Coaches />
-      <Services />
-      <Partners />
-      <Achievements />
-      <Gallery />
-      {/* <Legal /> */}
-      <Goals />
-      <Contacts />
+    <div style={{ backgroundColor: "honeydew" }}>
+      <section id="welcome">
+        <Navbar />
+        <Welcome />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="coaches">
+        <Coaches />
+      </section>
+      <section id="services">
+        <Services />
+      </section>
+      <section id="partners">
+        <Partners />
+      </section>
+      <section id="achievements">
+        <Achievements />
+      </section>
+      <section id="gallery">
+        <Gallery />
+      </section>
+      <section id="goals">
+        <Goals />
+      </section>
+      <section id="contact">
+        <Contacts />
+      </section>
     </div>
   );
 }
